8c5ff786e35cc5126f219b0324e542c8f5910d2a
Svit-PS <spolonchuk@gmail.com>
2024-01-07 21:09:22 +0200

    HomeWork-8

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,60 @@
 import { galleryItems } from "./gallery-items.js";
 
-// Change code below this line
+const galleryEl = document.querySelector(".js-gallery");
+galleryEl.addEventListener("click", onClickGallery);
 
-console.log(galleryItems);
-console.log("galleryItems: ", galleryItems);
+let lightBox;
+
+createGallery(galleryItems);
+
+function createGallery(arr) {
+  const str = arr
+    .map(({ preview: url, description: alt, original }) => {
+      return `<li class="gallery__item">
+          <a class="gallery__link">
+            <img
+              class="gallery__image"
+              href="${original}"
+              src="${url}"
+              data-source="${original}"
+              alt="${alt}"
+            />
+          </a>
+        </li>`;
+    })
+    .join("");
+
+  galleryEl.insertAdjacentHTML("beforeend", str);
+}
+
+function onClickGallery(event) {
+  const event_img = event.target;
+  if (!event_img.classList.contains("gallery__image")) {
+    return;
+  }
+  const urlEl = event_img.dataset.source;
+  const card = galleryItems.find(({ original }) => original === urlEl);
+
+  createModalCard(card);
+}
+
+function createModalCard(card) {
+  const modalDiv = `<img src="${card.original}" alt="${card.description}" width="800">`;
+
+  lightBox = basicLightbox.create(modalDiv, {
+    onShow: (lightBox) => {
+      document.addEventListener("keydown", onDownEsc);
+    },
+    onClose: (lightBox) => {
+      document.removeEventListener("keydown", onDownEsc);
+    },
+  });
+  lightBox.show();
+}
+
+function onDownEsc(event) {
+  //   console.log(event.code);
+  if (event.code === "Escape") {
+    lightBox.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".js-gallery");
+
+createGallery(galleryItems);
+
+function createGallery(arr) {
+  const str = arr
+    .map(({ preview: url, description: alt, original }) => {
+      return `<li class="gallery__item">
+          <a class="gallery__link">
+            <img
+              class="gallery__image"
+               href="${original}"
+              src="${url}"
+              alt="${alt}"
+            />
+          </a>
+        </li>`;
+    })
+    .join("");
+
+  galleryEl.insertAdjacentHTML("beforeend", str);
+}
